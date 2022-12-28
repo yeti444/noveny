2a83892a459ec39cd18b0b468c1ca2f6e7bfa32a
yeti444 <47927438+yeti444@users.noreply.github.com>
2022-12-28 21:15:13 +0100

    Update arduino_secrets.h
    Picit átformáltam

--- src/arduino_secrets.h
+++ src/arduino_secrets.h
@@ -1,2 +1,2 @@
-#define SECRET_OPTIONAL_PASS "***" //jelszó helye
-#define SECRET_SSID "***" //felhasználónév helye
+#define SECRET_OPTIONAL_PASS " *** " //jelszót kell írni a *** helyére
+#define SECRET_SSID " --- " //felhasználónevet kell beírni a --- helyére
